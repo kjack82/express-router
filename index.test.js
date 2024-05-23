0ae5b4ccf914f8d567c42215c7e995330bb415f3
kjack82 <kprothero1702@gmail.com>
2024-05-23 14:55:34 +0100

    "small changes"

--- index.test.js
+++ index.test.js
@@ -2,9 +2,3 @@ const request = require("supertest");
 const {describe, expect, it, test} = require
 ("@jest/globals")
 const app = require("./src/app")
-
-describe("the users endpoint", function ( {
-    it("returns all users", function ( {
-        
-    }))
-}))
